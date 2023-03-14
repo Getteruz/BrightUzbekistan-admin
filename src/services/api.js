7e37f86dd4ab58850938a162c2a81e40cf7c93d0
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-03-14 12:10:54 +0500

    c

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,14 @@ import axios from "axios";
 import { QueryClient } from "react-query";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL,
+    // withCredentials: true
 })
+
+api.interceptors.response.use(
+  response => response,
+  error => Promise.reject(error.response)
+)
 
 export const queryClient = new QueryClient({
     defaultOptions: {
--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ import { api } from "./api";
 
 export const login = async (data) => {
     try {
-        const res = await api.post('/auth/login', data, { withCredentials: true })
+        const res = await api.post('/auth/login', data, {withCredentials: true})
         console.log(res);
         return res?.data
     } catch (error) {
