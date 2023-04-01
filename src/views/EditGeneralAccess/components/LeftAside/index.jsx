bb622a6b6ab7ecf20ec06c5483f3e495e7b73c37
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-04-01 15:29:31 +0500

    fsdf

--- src/views/EditGeneralAccess/components/LeftAside/index.jsx
+++ src/views/EditGeneralAccess/components/LeftAside/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LeftAsideWrapper from '../../../../components/Aside/LeftAsideWrapper';
@@ -9,15 +10,39 @@ import UsersGroup from '../../../../components/UsersGroup';
 import useSocket from '../../../../hooks/useSocket';
 
 const LeftAside = ({ useForm }) => {
-    const navigate = useNavigate()
     const socket = useSocket()
-    const { getValues } = useForm
-    const value = getValues()
-    const editors = value?.editors?.map(edit => ({...edit.editor, lastEdited: edit.editedDate}))
+    const [editors, setEditors] = useState([])
+    const navigate = useNavigate()
+    const { watch } = useForm
+    const editorsFile = watch('editors')
+    console.log(editorsFile);
+    useEffect(() => {
+        socket.on('set_editor', data => {
+            
+        })
+        socket.on('input_focus', data => {
+            setEditors(state => {
+                return state.map(edit => {
+                    return data?.userId === edit?.editor?.id ?  {...edit, editor: {...edit?.editor, editing: true}} : edit
+                })
+            })
+        })
+        socket.on('input_blur', data => {
+            setEditors(state => {
+                return state.map(edit => {
+                    return data?.userId === edit?.editor?.id ?  {...edit, editor: {...edit?.editor, editing: false}} : edit
+                })
+            })
+        })
+    }, [])
 
     useEffect(() => {
-        socket.on('set_editor', data => console.log("data"))
-    })
+        setEditors(
+            editorsFile
+                ?.map(edit => ({id: edit?.id, editor: { ...edit.editor, lastEdited: edit.editedDate }}))
+                ?.sort((a, b) => new Date(b?.editor?.lastEdited) - new Date(a?.editor?.lastEdited))
+        )
+    }, [editorsFile])
     return (
         <LeftAsideWrapper>
             <RoundedButton onClick={() => navigate(-1)}>
@@ -25,9 +50,9 @@ const LeftAside = ({ useForm }) => {
                 Назад
             </RoundedButton>
             <Flex gap='15' direction='column' alignItems='flex-start'>
-                <UsersGroup label='Создатель' users={[value?.creator]} />
+                {/* <UsersGroup label='Создатель' users={[value?.creator]} /> */}
                 <p>Редактировали</p>
-                <UsersGroup label='Сегодня' users={editors} />
+                <UsersGroup label='Сегодня' users={editors?.map(edit => edit.editor)} />
             </Flex>
             <Flex gap='11' direction='column' alignItems='flex-start'>
                 <SimpleButton>Как создать?</SimpleButton>
